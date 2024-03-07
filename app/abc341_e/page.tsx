afece3fa073773be107b7d38b810c0d8c03c94ad
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-03-07 22:48:16 +0900

    inputMode="numeric"

--- app/abc341_e/page.tsx
+++ app/abc341_e/page.tsx
@@ -64,6 +64,7 @@ export default function ABC341_E() {
           <input
             name="problem_input_string"
             id="problem_input_string"
+            inputMode="numeric"
             className="block w-full rounded-md border-0 mt-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             value={text.join("")}
             onChange={handleTextChange}
