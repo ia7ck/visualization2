71164cb29bf88e1f636e691fff319883b5d81e61
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-03-13 02:50:45 +0900

    margin

--- app/abc323_f/page.tsx
+++ app/abc323_f/page.tsx
@@ -155,7 +155,7 @@ export default function ABC323_F() {
           >
             ABC323 F - Push and Carry
           </a>
-          <div className="grid place-items-center">
+          <div className="grid place-items-center mt-4">
             <button
               type="button"
               className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
@@ -217,7 +217,7 @@ export default function ABC323_F() {
               <span>{minActions}</span>
             </p>
           </div>
-          <div className="mt-2">
+          <div className="mt-4">
             <button
               type="button"
               className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-300"
