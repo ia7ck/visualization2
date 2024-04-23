47377bc7876a9032fd91156896d4ca13c8c541b4
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-04-23 21:49:38 +0900

    random

--- app/abc323_f/page.tsx
+++ app/abc323_f/page.tsx
@@ -162,7 +162,7 @@ export default function ABC323_F() {
               className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
               onClick={handleGachaButtonClick}
             >
-              Gacha
+              Random
             </button>
           </div>
           <div className="grid place-items-center mt-4">
