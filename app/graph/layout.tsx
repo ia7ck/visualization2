21d25ef5b9c52c1b2d338a0f52757ad01c79ea48
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-10-01 00:01:30 +0900

    reagrpah は canvas の位置を指定しにくそうなので削除

--- app/graph/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Graph",
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
